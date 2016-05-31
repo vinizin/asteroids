b158446ec54f99fa56a20091cf1422eac2c9472d
Fellipe Sombra <fellipe__sombra@hotmail.com>
2016-05-31 00:25:17 -0300

    Adicionado som no menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -4,15 +4,22 @@ using System.Collections;
 
 public class MenuManager : MonoBehaviour {
 
+	void Start()
+	{
+		AudioController.Play("MenuSong");
+	}
 
     public void LoadLevelSphere()
     {
+		AudioController.Stop("MenuSong");
         SceneManager.LoadScene("SphereLevel");
     }
 
     public void LoadLevelInfinite()
     {
+		AudioController.Stop("MenuSong");
         SceneManager.LoadScene("InfiniteLevel");
 
     }
+
 }
